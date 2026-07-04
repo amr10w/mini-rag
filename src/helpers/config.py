4233252e79407933550cd21dd6ffbaaf66019947
amr10w <amrw5189@gmail.com>
2026-07-04 12:46:26 +0300

    add configuration settings using Pydantic for environment variables

---
+++ src/helpers/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: str
+
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
+
+    class Config:
+        env_file = ".env"
+
+    def get_settings(self):
+        return Settings()
+        
