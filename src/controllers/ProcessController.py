a84875f35dcccc6486fcb9dc73a7ba4d456376ac
amr10w <amrw5189@gmail.com>
2026-07-05 16:23:52 +0300

    fix: correct instantiation of ProjectController in ProcessController

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -11,7 +11,7 @@ class ProcessController(BaseController):
     def __init__(self,project_id:str):
         super().__init__()
         self.project_id = project_id
-        self.project_path=ProjectController.get_project_path(
+        self.project_path=ProjectController().get_project_path(
                 project_id=self.project_id
         )
 
